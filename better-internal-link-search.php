dceee2c37b5c4d1a402236beb7ecab3ba04d4bd8
blazersix <blazersix@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-07-16 00:25:50 +0000

    Adding 1.1 with shortcut and modifier support
    git-svn-id: https://plugins.svn.wordpress.org/better-internal-link-search/trunk@572852 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- better-internal-link-search.php
+++ better-internal-link-search.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Better Internal Link Search
 Plugin URI: http://wordpress.org/extend/plugins/better-internal-link-search/
-Version: 1.0
-Description: Search by post or page title when adding links into the editor or adding pages to a nav menu.
+Version: 1.1
+Description: Improve the internal link popup functionality with time saving enhancements and features.
 Author: Blazer Six, Inc.
 Author URI: http://www.blazersix.com/
 License: GPLv2 or later
@@ -32,12 +32,14 @@ Blazer_Six_Better_Internal_Link_Search::start();
 
 
 class Blazer_Six_Better_Internal_Link_Search {
+	private static $s;
+	
 	/**
 	 * Start when plugins are loaded
 	 * 
 	 * @since 1.0
 	 */
-	function start() {
+	public static function start() {
 		add_action( 'plugins_loaded', array( __CLASS__, 'load_plugin' ) );
 	}
 
@@ -46,7 +48,12 @@ class Blazer_Six_Better_Internal_Link_Search {
 	 * 
 	 * @since 1.0
 	 */
-	function load_plugin() {
+	public static function load_plugin() {
+		if ( isset( $_POST['search'] ) ) {
+			remove_action( 'wp_ajax_wp-link-ajax', 'wp_link_ajax', 1 );
+			add_action( 'wp_ajax_wp-link-ajax', array( __CLASS__, 'ajax_get_link_search_results' ), 1 );
+			#add_action( 'wp_ajax_bils-get-link-search-results', array( __CLASS__, 'ajax_get_link_search_results' ) );
+		}
 		add_action( 'admin_init', array( __CLASS__, 'admin_init' ) );
 		add_action( 'admin_footer-post.php', array( __CLASS__, 'admin_footer' ) );
 		add_action( 'admin_footer-post-new.php', array( __CLASS__, 'admin_footer' ) );
@@ -60,11 +67,47 @@ class Blazer_Six_Better_Internal_Link_Search {
 	 * 
 	 * @since 1.0
 	 */
-	function admin_init() {
+	public static function admin_init() {
+		add_filter( 'better_internal_link_search_modifier-help', array( __CLASS__, 'search_modifier_help' ), 10, 2 );
+		
+		if ( add_filter( 'better_internal_link_search_load_default_modifiers', true ) ) {
+			include ( plugin_dir_path(__FILE__) . 'search-modifiers.php' );
+		}
+		
 		if ( defined('DOING_AJAX') && DOING_AJAX && isset( $_POST['action'] ) ) {
-			if ( 'menu-quick-search' == $_POST['action'] || 'wp-link-ajax' == $_POST['action'] ) {
+			$actions = array(
+				'bils-get-link-search-results',
+				'menu-quick-search',
+				'wp-link-ajax'
+			);
+			
+			if ( in_array( $_POST['action'], $actions ) ) {
 				add_filter( 'posts_search', array( __CLASS__, 'limit_search_to_title' ), 10, 2 );
+				add_action( 'pre_get_posts', array( __CLASS__, 'set_query_vars' ) );
 			}
+		}
+	}
+	
+	/**
+	 * Set query vars in pre_get_posts
+	 *
+	 * Includes scheduled posts in search results and disables paging
+	 *
+	 * @since 1.1
+	 */
+	public static function set_query_vars( $query ) {
+		if ( 'bils-get-link-search-results' == $_POST['action'] || 'wp-link-ajax' == $_POST['action'] ) {
+			// Scheduled post concept from Evan Solomon's plugin
+			// http://wordpress.org/extend/plugins/internal-linking-for-scheduled-posts/
+			$post_status = (array) $query->get( 'post_status' );
+			if ( ! in_array( 'future', $post_status ) ) {
+				$post_status[] = 'future';
+				$query->set( 'post_status', $post_status );
+			}
+			
+			// paging won't work with multiple data sources and ideally the search term
+			// should be unique enough that there aren't a ton of matches
+			$query->set( 'posts_per_page', -1 );
 		}
 	}
 	
@@ -75,7 +118,7 @@ class Blazer_Six_Better_Internal_Link_Search {
 	 * 
 	 * @since 1.0
 	 */
-	function limit_search_to_title( $search, &$wp_query ) {
+	public static function limit_search_to_title( $search, &$wp_query ) {
 		global $wpdb;
 		
 		if ( empty( $search ) ) {
@@ -101,21 +144,253 @@ class Blazer_Six_Better_Internal_Link_Search {
 	}
 	
 	/**
-	 * Javascript to automatically search for selected text
+	 * Returns search results
+	 *
+	 * Results returned in a format expected by the internal link manager.
+	 * Doesn't have support for paging.
+	 *
+	 * Multiple filters provided for either adding results or short-circuiting
+	 * the flow at various points.
+	 *
+	 * @since 1.1
+	 */
+	public static function ajax_get_link_search_results() {
+		global $wpdb;
+		
+		check_ajax_referer( 'internal-linking', '_ajax_linking_nonce' );
+		
+		if ( isset( $_POST['search'] ) ) {
+			$results = array();
+			$s = stripslashes( $_POST['search'] );
+			
+			$args['s'] = $s;
+			$args['page'] = ! empty( $_POST['page'] ) ? absint( $_POST['page'] ) : 1;
+			$args['per_page'] = 20; // default for usage in filters, otherwise, it shouldn't do anything
+			
+			// check to see if the request is prepended with a modifier (ex: -wikipedia interrobang, -spotify:artist willie nelson)
+			if ( 0 === mb_strpos( $s, '-' ) ) {
+				preg_match( '/-([^\s]+)\s?(.*)?/', $s, $matches );
+				
+				$s = trim( $matches[2] );
+				$args['s'] = $s;
+				$args['modifier'] = explode( ':', trim( $matches[1] ) );
+				
+				$results = (array) apply_filters( 'better_internal_link_search_modifier-' . $args['modifier'][0], array(), $args );
+				if ( ! empty( $results ) ) {
+					echo json_encode( $results );
+					wp_die();
+				}
+			}
+			
+			// allow plugins to intercept the request and add their own results or short-circuit execution
+			$pre_results = (array) apply_filters( 'pre_better_internal_link_search_results', array(), $args );
+			if ( ! empty( $pre_results ) ) {
+				$array_merge( $results, $pre_results );
+			}
+			
+			// don't continue if the query length is less than three
+			if ( strlen( $args['s'] ) < 3 ) {
+				wp_die( 0 );
+			}
+			
+			// @see wp_link_ajax();
+			require_once(ABSPATH . WPINC . '/class-wp-editor.php');
+			$posts = _WP_Editors::wp_link_query( $args );
+			if ( $posts ) {
+				$post_status_object = get_post_status_object( 'future' );
+				
+				foreach( $posts as $key => $post ) {
+					if ( 'future' == get_post_status( $post['ID'] ) ) {
+						$posts[ $key ]['info'] = $post_status_object->label;
+					}
+				}
+				
+				$results = array_merge( $results, $posts );
+			}
+			
+			// search for matching term archives
+			$search = '%' . like_escape( $s ) . '%';
+			$terms = $wpdb->get_results( $wpdb->prepare( "SELECT t.term_id, t.name, tt.taxonomy
+				FROM $wpdb->terms t
+				INNER JOIN $wpdb->term_taxonomy tt ON t.term_id=tt.term_id
+				WHERE t.name LIKE %s
+				ORDER BY name ASC", $search ) );
+			
+			if ( $terms ) {
+				foreach ( $terms as $term ) {
+					$taxonomy = get_taxonomy( $term->taxonomy );
+					
+					if ( $taxonomy->query_var ) {
+						$results[] = array(
+							'title' => trim( esc_html( strip_tags( $term->name ) ) ),
+							'permalink' => get_term_link( (int) $term->term_id, $term->taxonomy ),
+							'info' => $taxonomy->labels->singular_name
+						);
+					}
+				}
+			}
+			
+			// allow results to be filtered one last time and attempt to sort them
+			if ( ! empty( $results ) ) {
+				self::$s = $s;
+				$results = apply_filters( 'better_internal_link_search_results', $results, $args );
+				
+				if ( apply_filters( 'better_internal_link_search_sort_results', true, $results, $args ) ) {
+					usort( $results, array( __CLASS__, 'sort_results' ) );	
+				}
+			}
+			
+			// add shortcut results
+			$shortcuts = (array) self::get_shortcuts();
+			if ( ! empty( $shortcuts ) ) {
+				if ( array_key_exists( $s, $shortcuts ) ) {
+					array_unshift( $results, $shortcuts[ $s ] );
+				} elseif ( 'shortcuts' == $s ) {
+					$results = array_merge( $shortcuts, $results );	
+				}
+			}
+		}
+		
+		if ( ! isset( $results ) || empty( $results ) ) {
+			wp_die( 0 );
+		}
+		
+		echo json_encode( $results );
+		echo "\n";
+		wp_die();
+	}
+	
+	/**
+	 * Internal link shortcuts
+	 *
+	 * A couple of basic shortcuts for easily linking to the home url and site
+	 * url. Also gives plugins the ability to add more shortcuts.
+	 *
+	 * @since 1.1
+	 */
+	public static function get_shortcuts() {
+		$shortcuts = apply_filters( 'better_internal_link_search_shortcuts', array(
+			'home' => array(
+				'title' => 'Home',
+				'permalink' => home_url( '/' )
+			),
+			'siteurl' => array(
+				'title' => 'Site URL',
+				'permalink' => site_url( '/' )
+			)
+		) );
+		
+		if ( ! empty( $shortcuts ) ) {
+			// sanitize the shortcuts a bit
+			foreach( $shortcuts as $key => $shortcut ) {
+				if ( empty( $shortcut['title'] ) || empty( $shortcut['permalink'] ) ) {
+					unset( $shortcuts[ $key ] );
+					break;
+				}
+				
+				if ( empty( $shortcut['info'] ) ) {
+					$shortcuts[ $key ]['info'] = 'Shortcut';
+				}
+				
+				$shortcuts[ $key ]['title'] = trim( esc_html( strip_tags( $shortcut['title'] ) ) );
+				$shortcuts[ $key ]['info'] = trim( esc_html( strip_tags( $shortcuts[ $key ]['info'] ) ) );
+			}
+		}
+		
+		return $shortcuts;
+	}
+	
+	/**
+	 * Custom results sorter
 	 * 
+	 * Attempts to return results in a more natural order. Titles that exactly
+	 * match a search query are returned first, followed by titles that begin
+	 * with the query. Remaining results are sorted alphabetically.
+	 *
+	 * TODO: Potentially remove articles (a, an, the) when doing matches.
+	 *
+	 * @since 1.1
+	 */
+	public static function sort_results( $a, $b ) {
+		$a_title = mb_strtolower( $a['title'] );
+		$b_title = mb_strtolower( $b['title'] );
+		$s = mb_strtolower( self::$s );
+		
+		if ( $a_title == $b_title ) {
+			return 0;
+		}
+		
+		if ( $s == $a_title ) {
+			return -1;
+		} elseif ( $s == $b_title ) {
+			return 1;
+		}
+		
+		$a_strpos = mb_strpos( $a_title, $s );
+		$b_strpos = mb_strpos( $b_title, $s );
+		if ( 0 === $a_strpos && 0 === $b_strpos ) {
+			// return the shorter title first
+			return ( mb_strlen( $a_title ) < mb_strlen( $b_title ) ) ? -1 : 1;
+		} elseif ( 0 === $a_strpos ) {
+			return -1;
+		} elseif ( 0 === $b_strpos ) {
+			return 1;	
+		}
+		
+		return strcmp( $a_title, $b_title );
+	}
+	
+	/**
+	 * Javascript to automatically search for text selected in the editor
+	 *
 	 * Inserts any text selected in the editor into the search field in the
 	 * "Insert/edit link" popup when the link button in the toolbar is
 	 * clicked. Automatically executes a search request and returns the
 	 * results.
-	 * 
+	 *
 	 * @since 1.0
 	 */
-	function admin_footer() {
+	public static function admin_footer() {
 		?>
 		<script type="text/javascript">
 		jQuery(function($) {
+			$.ajaxPrefilter(function(options, originalOptions, jqXHR) {
+				if ( -1 != options.data.indexOf('action=wp-link-ajax') && -1 != options.data.indexOf('search=') ) {
+					// abort the request if it's just for resetting the river
+					if ( -1 != options.data.indexOf('better-internal-link-search-reset-river-flag') ) {
+						jqXHR.abort();
+					}
+					
+					// reset the search field to a single dash
+					if ( -1 != options.data.indexOf('search=-help') ) {
+						$('#search-field').val('-');
+					}
+				}
+			});
+				
 			$('#wp-link').bind('wpdialogbeforeopen', function() {
-				var searchTerm = '';
+				var searchField = $('#search-field').width(200),
+					searchTerm = '-',
+					timeout;
+				
+				// don't mind me, just debouncing, yo
+				searchField.off('keyup').on('keyup.bils', function() {
+					var self = this
+						$self = $(this);
+					
+					clearTimeout(timeout);
+					timeout = setTimeout( function() {
+						if ( '-' == $self.val() || 0 === $self.val().indexOf('-help') ) {
+							// ugly hack to reset the river...
+							$self.val('better-internal-link-search-reset-river-flag');
+							wpLink.searchInternalLinks.apply( self );
+							// and then bypass the three character minimum requirement
+							$self.val('-help');
+						}
+						
+						wpLink.searchInternalLinks.apply( self );
+					}, 500 );
+				});
 				
 				// determine what text is selected in the editor
 				if ( 'undefined' != typeof tinyMCE && ( editor = tinyMCE.activeEditor ) && ! editor.isHidden() ) {
@@ -130,7 +405,7 @@ class Blazer_Six_Better_Internal_Link_Search {
 						end = wpLink.textarea().selectionEnd;
 					
 					if ( 0 < end-start ) {
-						searchTerm = wpLink.textarea().value.substring( start, end );
+						searchTerm = wpLink.textarea().value.substring(start, end);
 					}
 				}
 				
@@ -140,12 +415,40 @@ class Blazer_Six_Better_Internal_Link_Search {
 				}
 				
 				if ( searchTerm.length ) {
-					$('#search-field').val( $.trim(searchTerm) ).keyup();
+					searchField.val( $.trim(searchTerm) ).keyup();
 				}
 			});
 		});
 		</script>
+		<style type="text/css">
+		#wp-link .item-description { display: block; padding: 3px 0 0 10px;}
+		#wp-link .link-search-wrapper img.waiting { margin-top: 5px;}
+		#wp-link .link-search-wrapper input { float: left;}
+		#wp-link .link-search-wrapper span { margin-top: 0; padding-top: 5px; line-height: 15px;}
+		</style>
 		<?php
+	}
+	
+	/**
+	 * Search modifier help
+	 *
+	 * Intercepts a request for '-help' and displays any modifiers that have
+	 * been added via the filter.
+	 *
+	 * @since 1.1
+	 */
+	public static function search_modifier_help( $results, $args ) {
+		if ( intval( $args['page'] ) > 1 ) {
+			return array();
+		}
+		
+		$results = apply_filters( 'better_internal_link_search_modifier_help', array() );
+		if ( ! empty( $results ) && ! empty( $args['s'] ) && array_key_exists( $args['s'], $results ) ) {
+			// if the -help request has a search query, limit the returned results to that modifier
+			$results = array( $results[ $args['s'] ] );
+		}
+		
+		return $results;
 	}
 }
 ?>
